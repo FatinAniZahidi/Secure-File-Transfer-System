14967284b966a388a9fbbd8c416d1ff23b4484ad
shakirfazrie <shakirfazrie@gmail.com>
2021-07-07 18:04:26 +0000

    Eighth Commit

--- ftserver.py
+++ ftserver.py
@@ -31,6 +31,61 @@ class Server:
             threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
     def handle_client(self,c,addr):
+        New = c.recv(1024)  #receive input user from client
+
+        if New.decode() == "y":   #if new user
+            with open("login.txt", "a+") as register:
+                username = c.recv(1024).decode()
+                password = c.recv(1024).decode()
+
+                #save new username & password into login.txt file
+                register.seek(0)
+                register.write(username)
+                register.write(":")
+
+                register.write(password)
+                register.write('\n')
+
+                #
+                c.send("continue".encode())
+
+            #receive username & password new registered user
+            with open("login.txt", "r") as login:
+                username = c.recv(1024).decode()
+                password = c.recv(1024).decode()
+
+                #check either the login info exist in login.txt or not
+                jumpa = "tak jumpa";
+                for line in login:
+                    creds = line.strip()
+                    if creds.split(":")[0] in username and creds.split(":")[1] in password:
+                        jumpa = "jumpa";
+
+                if jumpa == "jumpa":
+                    c.send(("\tWelcome New User : %s" % (username)).encode())
+                else:
+                    c.send("Not-a-user".encode())
+                    #c.close()
+
+        else:   #if not a new user
+            with open("login.txt", "r") as login:
+                username = c.recv(1024).decode()
+                password = c.recv(1024).decode()
+
+                jumpa = "tak jumpa";
+                for line in login:
+                    creds = line.strip()
+                    if creds.split(":")[0] in username and creds.split(":")[1] in password:
+                        jumpa = "jumpa";
+
+                if jumpa == "jumpa":
+                    c.send(("\tWelcome back: %s" % (username)).encode())
+                else:
+                    c.send("Not-a-user".encode())
+                    #c.close()
+
+
+    def handle_client(self,c,addr):
         data = c.recv(1024).decode()
     
         if not os.path.exists(data):
